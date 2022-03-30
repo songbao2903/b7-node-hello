da02a077762cb8a3ed585494669fd72a26ee822d
songbao2903 <93124426+songbao2903@users.noreply.github.com>
2022-03-31 06:54:03 +0700

    Update push.sh

--- jenkins/push.sh
+++ jenkins/push.sh
@@ -1,10 +1,9 @@
-
-
 export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
 export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}
 export AWS_DEFAULT_OUTPUT=${AWS_DEFAULT_OUTPUT}
-            
+
+           
 #ECR_LOGIN_STRING=`aws ecr get-login --region ${AWS_DEFAULT_REGION} --no-include-email`
 #aws ecr get-login --no-include-momnhuong@gmail.com --region ap-southeast-1 
 docker login --username AWS --password-stdin $(aws ecr get-login-password --region ap-southeast-1) 709026135780.dkr.ecr.ap-southeast-1.amazonaws.com/nodejs
